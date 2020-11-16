de86b51f58235c4bc9f96dd249028cdc0dadaae2
Maigal <matiasigalmeida@gmail.com>
2020-11-16 03:05:47 -0300

    trap test

--- client/Death/DeathController.gd
+++ client/Death/DeathController.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-var BloodParticle = load("res://Death/DeathParticle.tscn")
-export var BloodParticleNumber = 10
+var DeathParticle = load("res://Death/DeathParticle.tscn")
+export var DeathParticleNumber = 10
 export var RandomVelocity = 800.0
 
 
@@ -11,12 +11,12 @@ var rnd = RandomNumberGenerator.new()
 func explode(particles_to_spawn = -1):
 	print('SPLATTER')
 	if (particles_to_spawn <= 0):
-		particles_to_spawn = BloodParticleNumber
+		particles_to_spawn = DeathParticleNumber
 		
 		var spawnedParticle: RigidBody2D
 		
 		for i in range(particles_to_spawn):
-			spawnedParticle = BloodParticle.instance()
+			spawnedParticle = DeathParticle.instance()
 			get_tree().root.add_child(spawnedParticle)
 			spawnedParticle.global_position = global_position
 			spawnedParticle.linear_velocity = Vector2(rnd.randf_range(800, 600), rnd.randf_range(-100, 100))
--- client/Death/DeathParticle.gd
+++ client/Death/DeathParticle.gd
@@ -1,29 +1,5 @@
 extends RigidBody2D
 
-var prevX = position.x
-var prevY = position.y
-var dir = Vector2.ZERO
-
-
-func _physics_process(delta):
-	if position.x > prevX:
-		dir.x = 1
-	elif position.x < prevX:
-		dir.x = -1
-	else:
-		dir.x = 0
-		
-	if position.y > prevY:
-		dir.y = 1
-	elif position.y < prevY:
-		dir.y = -1
-	else:
-		dir.y = 0
-		
-	prevX = position.x
-	prevY = position.y
-
 func _on_BloodParticle_body_entered(body):
-	get_tree().call_group("room", "draw_blood_splatter", position, linear_velocity, dir, Color(1.0,0,0,1.0))
 	yield(get_tree().create_timer(1.0), "timeout")
 	queue_free()
